8b6b1083796fcbea16860c78315ea25d015a33c3
DEV-HANDSON <job4pjs@gmail.com>
2020-10-07 13:12:15 -0500

    Initial Checkin

---
+++ src/test/java/com/atm/demo/ordermgmt/OrderManagementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atm.demo.ordermgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
